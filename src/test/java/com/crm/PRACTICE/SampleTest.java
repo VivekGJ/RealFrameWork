3e694ce76579c3cacbfe96d5acd990d8d9e2216f
vivekgj <vivekanandagowdaj@gmail.com>
2023-04-20 22:03:38 +0530

    added new testcase101

---
+++ src/test/java/com/crm/PRACTICE/SampleTest.java
@@ -0,0 +1,22 @@
+package com.crm.PRACTICE;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SampleTest
+{
+    @Test
+    public void demoTest()
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://ui.cogmento.com/");
+
+    }
+}
